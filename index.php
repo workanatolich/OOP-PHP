3e8bf2f58c3fd3d83f411fc1d225aa2d097b73a9
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-30 01:26:02 +0300

    Lesson 3. Working with class methods

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
 
-class Employee {
+class User {
     public $name;
     public $age;
-    public $salary;
+
+    public function show($str) {
+        echo $str .'!!!';
+    }
 }
 
-$employee1 = new Employee;
-$employee1->name = 'John';
-$employee1->age = 20;
-$employee1->salary = 1500;
-
-$employee2 = new Employee;
-$employee2->name = 'Matt';
-$employee2->age = 25;
-$employee2->salary = 2500;
-
-echo $employee1->age + $employee2->age;
-echo '<br>';
-echo $employee1->salary + $employee2->salary;
-
+$user = new User;
+$user->name = 'Kira';
+$user->age = 27;
+$user->show('hello');
