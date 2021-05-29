2352944a695c706f47ba51298195daee81ef1c86
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-30 00:36:01 +0300

    Lesson 2. Working with class properties

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
 
-class Car {
-    public $color;
-    public $fuel;
-
-    public function go() {
-
-    }
-
-    public function turn() {
-
-    }
-
-    public function stop() {
-
-    }
+class Employee {
+    public $name;
+    public $age;
+    public $salary;
 }
 
-$myCar = new Car;
-$myCar -> color = 'red';
-$myCar -> fuel = 50;
+$employee1 = new Employee;
+$employee1->name = 'John';
+$employee1->age = 20;
+$employee1->salary = 1500;
 
-$myCar -> go();
-$myCar -> turn();
-$myCar -> stop();
+$employee2 = new Employee;
+$employee2->name = 'Matt';
+$employee2->age = 25;
+$employee2->salary = 2500;
+
+echo $employee1->age + $employee2->age;
+echo '<br>';
+echo $employee1->salary + $employee2->salary;
+
