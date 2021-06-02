53d5fa6518212461ef3004f111277df51e01df5c
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-06-02 22:22:47 +0300

    Lesson 7. Setter && getter

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
 <?php
 
 class Employee {
-    public $name;
-    public $salary;
+    private $name;
+    private $age;
+    private $salary;
 
-    public function __construct($name, $salary) {
-       $this->name = $name;
-       $this->salary = $salary;
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAge($age) {
+        if($this->isAgeCorrect($age)) {
+            $this->age = $age;
+        } else {
+            echo 'Неверный возраст';
+        }
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setSalary($salary) {
+        $this->salary =$salary;
+    }
+
+    public function getSalary() {
+        return "$this->salary \$";
+    }
+
+    private function isAgeCorrect($age) {
+        return $age > 1 && $age <= 100;
     }
 
 }
 
-$emp1 = new Employee('John', 200);
-$emp2 = new Employee('Matt', 300);
+$emp = new Employee();
+$emp->setName('John');
+$emp->setAge(30);
+$emp->setSalary(400);
 
-echo $emp1->salary+$emp2->salary;
+echo $emp->getSalary();
